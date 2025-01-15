027ccda0a2a1857274c5c94f626743a102747f69
jasper_zh <1839646816@qq.com>
2025-01-15 18:02:24 +0800

    langfuse应用

--- langfuse/demo.py
+++ langfuse/demo.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())  # 加载.env进环境变量
 
 
-from langfuse.decorators import observe
+from langfuse.decorators import observe, langfuse_context
 from langfuse.openai import openai  # OpenAI integration
 
 @observe(name="大模型交互")
@@ -28,6 +28,10 @@ def story():
 
 @observe(name="查询数据库")
 def queryData():
+    # @oberve的属性是标记span的属性
+    # langfuse_context.update_current_trace是标记当前trace的属性
+    # 第一个span的属性为trace属性，但中途也能通过该函数修改trace属性
+    langfuse_context.update_current_trace(name="一次RAG", user_id="001")
     return "一些数据"
 
 
